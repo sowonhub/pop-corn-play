814a8bba8ff243b05bd683b9d0fc6bf4e5041b9b
kimsowon <sowon836@gmail.com>
2025-11-07 02:42:09 +0900

    chore/3--setup-tailwind-prettier-eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black">
+      <p className="rounded-lg p-3 text-white shadow">Tailwind OK</p>
+    </main>
+  );
 }
-
-export default App;
